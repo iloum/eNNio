faa65a76a81b9e3c30fccddff838a2d46e01211e
Ioannis Loumiotis <i_loumiotis@outlook.com>
2019-12-08 16:45:46 +0200

    modified classes

---
+++ feature_extractor/features_extractor.py
@@ -0,0 +1,17 @@
+
+
+class FeatureExtractor(object):
+    def __init__(self, path):
+        self.path = path
+
+    def extract_features(self):
+        """
+        Method to extract features from a file
+        """
+        raise NotImplementedError
+
+
+
+
+
+
--- feature_extractor/video_feature_exractor.py
+++ feature_extractor/video_feature_exractor.py
@@ -1,5 +1,12 @@
-class VideoFeatureExtractor:
-    def __init__(self):
+import feature_extractor.video_features as vf
+import numpy as np
+from utilities import file_management as fm
+from feature_extractor.features_extractor import FeatureExtractor
+
+
+class VideoFeatureExtractor(FeatureExtractor):
+    def __init__(self, videopath):
+        super().__init__(videopath)
         pass
 
     def extract_video_features(self, filename):
@@ -8,4 +15,24 @@ class VideoFeatureExtractor:
         :param filename: Name of the video file
         :return: Numpy array containing video features
         """
-        pass
+
+        v = vf.VideoFeatureExtractor(["lbps", "hogs", "colors", "flow"], resize_width=300, step=3)
+        features, time, featureNames = v.extract_features(filename)
+        featuresAvg = np.average(features, axis=0)
+        return featuresAvg, featureNames
+
+
+    def extract_features(self):
+        filelist = fm.getfilelist(self.path)
+        videoFeatures = list()
+        for file in filelist:
+            featuresAvg, featureNames = self.extract_video_features(file)
+            videoFeatures.append(featuresAvg)
+        videoFeatures.append(featureNames)
+        return videoFeatures
+
+# for my testing
+if __name__=="__main__":
+    fe = VideoFeatureExtractor("D:\\test")
+    r = fe.extract_features()
+    print(r)
