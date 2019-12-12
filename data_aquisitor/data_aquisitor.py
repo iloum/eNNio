de7707306b16717a9724460fc8ad252c3b985c3d
Konstantinos Gyftakis <gyftakiskon@gmail.com>
2019-12-12 15:48:50 +0200

    API changes

--- data_aquisitor/data_aquisitor.py
+++ data_aquisitor/data_aquisitor.py
@@ -1,12 +1,17 @@
 class DataAquisitor:
     def __init__(self):
-        pass
+        self._download_location = None
 
-    def download_from_list(self, url_list):
+    def set_download_location(self, path):
+        self._download_location = path
+
+    def download_from_url(self, url, start_time=None, end_time=None):
         """
         Method to download youtube videos from a URL list
-        :param url_list: List of tuples (url, start_time, end_time)
-        :return: List of tuples (id, filename)
+        :param url: Youtube video URL
+        :param start_time: Starting time of the scene
+        :param end_time: Ending time of the scene
+        :return: filename
         """
         pass
 
