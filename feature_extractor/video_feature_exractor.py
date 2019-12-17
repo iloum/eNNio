8d9518169f07ad21b492f6559f536519762c6c2a
Ioannis Loumiotis <i_loumiotis@outlook.com>
2019-12-17 23:20:53 +0200

    modifications in video

--- feature_extractor/video_feature_exractor.py
+++ feature_extractor/video_feature_exractor.py
@@ -1,6 +1,5 @@
 import feature_extractor.video_features as vf
 import numpy as np
-from utilities import file_management as fm
 from feature_extractor.features_extractor import FeatureExtractor
 
 
@@ -8,6 +7,8 @@ class VideoFeatureExtractor(FeatureExtractor):
     def __init__(self, videopath):
         super().__init__(videopath)
         pass
+    #TODO:
+    #Remove path from constructor. Check george code to be the same and then remove it.
 
     def extract_video_features(self, filename):
         """
@@ -15,24 +16,15 @@ class VideoFeatureExtractor(FeatureExtractor):
         :param filename: Name of the video file
         :return: Numpy array containing video features
         """
-
         v = vf.VideoFeatureExtractor(["lbps", "hogs", "colors", "flow"], resize_width=300, step=3)
         features, time, featureNames = v.extract_features(filename)
         featuresAvg = np.average(features, axis=0)
         return featuresAvg, featureNames
 
 
-    def extract_features(self):
-        filelist = fm.getfilelist(self.path)
-        videoFeatures = list()
-        for file in filelist:
-            featuresAvg, featureNames = self.extract_video_features(file)
-            videoFeatures.append(featuresAvg)
-        videoFeatures.append(featureNames)
-        return videoFeatures
 
 # for my testing
 if __name__=="__main__":
-    fe = VideoFeatureExtractor("D:\\test")
-    r = fe.extract_features()
+    fe = VideoFeatureExtractor("D:\\test\\Zoobi Doobi 3 Idiots Full Song Feat. Aamir Khan, Kareena Kapoor-yJ1uLVgv3Vg.mp4")
+    r, n = fe.extract_video_features("D:\\test\\Zoobi Doobi 3 Idiots Full Song Feat. Aamir Khan, Kareena Kapoor-yJ1uLVgv3Vg.mp4")
     print(r)
