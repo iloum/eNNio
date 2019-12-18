f0fb1967337bde72f331e337094b3dd4a7a38141
George Touros <george.touros@marinetraffic.com>
2019-12-18 14:20:29 +0200

    Quick audio feature extraction, using pyAudioAnalysis. The method allows extraction per file. Needs mid-term and short-term window to work. Returns long-term average

--- feature_extractor/audio_feature_extractor.py
+++ feature_extractor/audio_feature_extractor.py
@@ -1,23 +1,47 @@
-
-from pyAudioAnalysis.MidTermFeatures import directory_feature_extraction as dW
+import os
+from pyAudioAnalysis.MidTermFeatures import mid_feature_extraction, beat_extraction
+from pyAudioAnalysis import audioBasicIO
+import numpy as np
 
 
 class AudioFeatureExtractor:
     def __init__(self):
         pass
 
-    def extract_audio_features(self, folder_name):
+    def extract_audio_features(self,
+                               file_path,
+                               mid_window=1,
+                               mid_step=1,
+                               short_window=0.05,
+                               short_step=0.05,):
         """
         Method to extract audio features from an audio file
-        :param folder_name: Name of the folder with the audio files
-        :return: Numpy array containing audio features
+        :param file_path: Name of the audio file
+        :param mid_window: the mid-term window in seconds
+        :param mid_step: the mid-term step
+        :param short_window: the short-term window
+        :param short_step: the short-term step
+        :return: Numpy array containing audio features, list of mid-feature names
         """
-        # get features from folders (all classes):
-        f1, _, fn1 = dW(folder_path=folder_name,
-                        mid_window=1,
-                        mid_step=1,
-                        short_window=0.05,
-                        short_step=0.05,
-                        compute_beat=True)
-        return f1
+        # check for empty file
+        if os.stat(file_path).st_size == 0:
+            print("   (EMPTY FILE -- SKIPPING)")
+            # read sampling rate
+        [sampling_rate, signal] = audioBasicIO.read_audio_file(file_path)
+
+        mid_features, short_features, mid_feature_names = \
+            mid_feature_extraction(signal, sampling_rate,
+                                   round(mid_window * sampling_rate),
+                                   round(mid_step * sampling_rate),
+                                   round(sampling_rate * short_window),
+                                   round(sampling_rate * short_step))
+        beat, beat_conf = beat_extraction(short_features, short_step)
+        mid_features = np.transpose(mid_features)
+        # long term averaging of mid-term statistics
+        mid_features = mid_features.mean(axis=0)
+        if (not np.isnan(mid_features).any()) and \
+                (not np.isinf(mid_features).any()):
+            mid_features = np.append(mid_features, beat)
+            mid_features = np.append(mid_features, beat_conf)
+        return mid_features
 
