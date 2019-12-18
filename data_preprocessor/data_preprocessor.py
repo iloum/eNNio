1baee04a702d18a591cde9ab85537323fb29b51a
Ioannis Loumiotis <i_loumiotis@outlook.com>
2019-12-19 00:02:59 +0200

    simple preprocessor implementing PCA

--- data_preprocessor/data_preprocessor.py
+++ data_preprocessor/data_preprocessor.py
@@ -1,3 +1,6 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
 class DataPreprocessor:
     def __init__(self):
         pass
@@ -9,3 +12,20 @@ class DataPreprocessor:
         :return: Preprocessed Dataframe
         """
         pass
+
+
+def reduce_dimensions(data, newdimension):
+    """
+    Method to reduce the size of the data
+    :param data: the data to be reduced
+    :param newdimension: the new size of the data
+    :return: the reduced data, the scaler used and the pca
+    """
+    scaler = StandardScaler()
+    scaler.fit(data)
+    scaleddata = scaler.transform(data)
+
+    pca = PCA(n_components=newdimension)
+    pca.fit(scaleddata)
+    newdata = pca.transform(scaleddata)
+    return newdata, scaler, pca
