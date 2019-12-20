280c0ef802b5a679d63d2c03ba3ed68bd26af1b3
Ioannis Loumiotis <i_loumiotis@outlook.com>
2019-12-20 22:15:29 +0200

    modifications in ml core

--- ml_core/ml_core.py
+++ ml_core/ml_core.py
@@ -1,8 +1,20 @@
-import  pickle
+import pickle
+from ml_core.ml_ANN import ANN
+from utilities import file_management as fm
 
 class MLCore:
-    def __init__(self, modelcls):
-        self.modelclass = modelcls  # the class of the model
+    def __init__(self, mdl):
+        '''
+        Constructor
+        :param mdl: the name of the model (string)
+        '''
+        self.name = mdl
+        storedmodels = fm.getfiledictionary(path="D:\\Test\\models")  # to be changed
+        if mdl == "ANN":
+            if mdl in storedmodels.keys():  # if model exist, load it
+                self.modelclass = pickle.load(open(storedmodels[mdl], "rb"))
+            else:  # else create it
+                self.modelclass = ANN("ANN", batch_size=64, epochs=15, inputsize=554, outputsize=10)
         pass
 
     def train_model(self, train_data):
@@ -33,11 +45,11 @@ class MLCore:
         y_predict = self.modelclass.predict_ml_model(video_features)
         return y_predict
 
-    def save_ml_core(self,filename):
+    def save_ml_core(self):
         """
         Method to save trained model
         :param filename: Name of the file to save model to
         :return:
         """
-        pickle.dump(self, open(filename, "wb"))
+        pickle.dump(self.modelclass, open(self.name, "wb"))
         return True
--- utilities/file_management.py
+++ utilities/file_management.py
@@ -5,3 +5,11 @@ from os.path import isfile, join
 def getfilelist(path):
     files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
     return files
+
+
+def getfiledictionary(path):
+    filedict = dict()
+    for f in listdir(path):
+        if isfile(join(path, f)):
+            filedict[f] = join(path, f)
+    return filedict
