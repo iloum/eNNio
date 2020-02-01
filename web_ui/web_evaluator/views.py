f2d12fc3f602c09b4aa0b8d7af50561d9fa14e21
GTouros <tourosgeorge@gmail.com>
2020-02-02 01:43:38 +0200

    Create evaluator core pages - not functional yer

--- web_ui/web_evaluator/views.py
+++ web_ui/web_evaluator/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Thanks for helping us make ennIO better. Let\'s get started!</h1>')
+    return render(request, 'web_evaluator/home.html')
+
 
 def user_input(request):
-    return HttpResponse('<h1> Please input your Youtube URL</h1>')
+    return render(request, 'web_evaluator/user-input.html')
