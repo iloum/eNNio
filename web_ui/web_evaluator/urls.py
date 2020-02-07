e7b125a28ee945ce7c52a98aed40bad37bef8bbe
GTouros <tourosgeorge@gmail.com>
2020-02-07 21:48:05 +0200

    Initial javascript functions

--- web_ui/web_evaluator/urls.py
+++ web_ui/web_evaluator/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='evaluator-home'),
     path('input/', views.user_input, name='evaluator-input'),
+    path('input/hello-world', views.hello_world, name='evaluator-hello'),
     path('wait/', views.wait, name='wait')
 ]
--- web_ui/web_evaluator/views.py
+++ web_ui/web_evaluator/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
@@ -9,6 +11,18 @@ def home(request):
 def user_input(request):
     return render(request, 'web_evaluator/user-input.html', {'title': 'Input URL'})
 
+@csrf_exempt
+def hello_world(request):
+    url_input = request.POST.get('url_input', None)
+    text_input = request.POST.get('text_input', None)
+    #Kanw logikous elegxous sta data
+
+
+    data = {
+        'error': True,
+        'response': 'Hey Touros',
+    }
+    return JsonResponse(data)
 
 def wait(request):
     return render(request, 'web_evaluator/wait.html', {'title': 'Please Wait...'})
