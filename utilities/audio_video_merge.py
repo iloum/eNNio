838b121263293f4686eea6a73d505692a1255a8d
Nick <nikolaounick@hotmail.com>
2020-02-09 21:03:55 +0200

    predict and join TODO: live mode TODO: exceptions

--- utilities/audio_video_merge.py
+++ utilities/audio_video_merge.py
@@ -1,3 +1,6 @@
-def audio_video_merge(audio_path,video_path,export_path):
+import os
+
+
+def audio_video_merge(audio_path, video_path, export_path):
     os.system(f"ffmpeg -y -t 60 -i {video_path} -i {audio_path} \
                     -c:v copy -c:a aac -strict experimental {export_path}")
