4bc51445d1a255ee8975297b9be8e2e66beed566
Zappatistas <tourosgeorge@gmail.com>
2020-02-15 02:23:11 +0200

    reversed silly changes that made it unstable. Please merge with caution...

--- web_ui/settings.py
+++ web_ui/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from ennio_core.ennio_core import EnnIOCore
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+EnnIOCore(BASE_DIR).setup()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
